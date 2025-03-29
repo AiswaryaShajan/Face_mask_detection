fcfb4b1eef1fbeea0a1e949bc47a4af81ea1e32a
AiswaryaShajan <AS4213@live.mdx.ac.uk>
2025-03-29 12:44:53 +0000

    Import libraries

--- train.py
+++ train.py
@@ -1,2 +1,12 @@
-# Your training script starts here
-print("Hello, this is train.py")
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import cv2
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Input, ZeroPadding2D, BatchNormalization, Activation, MaxPooling2D, Flatten, Dense,Dropout
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+from sklearn.utils import shuffle
+import imutils
+import numpy as np
